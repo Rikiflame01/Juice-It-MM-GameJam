2e7024b0ed6abd0d66cfd84e804dbc2d4c52da2b
Coolguybrad <bnwicks03@gmail.com>
2025-01-25 16:21:36 +0200

    miniPush

---
+++ MakersMassiveJuiceIt/Assets/!Scripts/Managers/SliderManager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+public class SliderManager : MonoBehaviour
+{
+
+    [SerializeField] private Slider sldJuice;
+    [SerializeField] private float drainDuration = 1;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine("drainBar");
+        }
+    }
+
+    public void IncrementSlider(float fruitVal)
+    {
+        if (sldJuice.value >= sldJuice.maxValue)
+        {
+            StartCoroutine(drainBar());
+        }
+        else
+        {
+            sldJuice.value += fruitVal;
+        }
+
+    }
+
+    public IEnumerator drainBar()
+    {
+        float startValue = sldJuice.value;
+        float elapsedTime = 0f;
+
+        //yield return new WaitForSecondsRealtime(5);
+
+        while (elapsedTime < drainDuration)
+        {
+            sldJuice.value = Mathf.Lerp(startValue, 0, elapsedTime / drainDuration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        sldJuice.value = 0; // Ensure the slider value is set to 0 at the end
+    }
+
+
+
+
+}
