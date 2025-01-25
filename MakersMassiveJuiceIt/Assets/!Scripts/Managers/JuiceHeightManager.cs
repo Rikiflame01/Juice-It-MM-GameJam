dae02059939dabee601dc82ec8834cdfad0153a8
Coolguybrad <bnwicks03@gmail.com>
2025-01-25 17:41:54 +0200

    The level fills :)

--- MakersMassiveJuiceIt/Assets/!Scripts/Managers/JuiceHeightManager.cs
+++ MakersMassiveJuiceIt/Assets/!Scripts/Managers/JuiceHeightManager.cs
@@ -4,6 +4,8 @@ public class JuiceHeightManager : MonoBehaviour
 {
     [SerializeField] private Slider sldJuice;
     [SerializeField] private GameObject juice;
+    [SerializeField] private float juiceMinHeight = 0.46f;
+    [SerializeField] private float juiceMaxHeight = 1;
     
     void Start()
     {
@@ -14,7 +16,11 @@ public class JuiceHeightManager : MonoBehaviour
     void Update()
     {
         Vector3 juicePosition = juice.transform.position;
-        juicePosition.y = sldJuice.value; 
+        juicePosition.y = sldJuice.value;
         juice.transform.position = juicePosition;
+
+        //Vector3 juicePosition = juice.transform.position;
+        //juicePosition.y = Mathf.Lerp(juiceMinHeight, juiceMaxHeight, sldJuice.value);
+        //juice.transform.position = juicePosition;
     }
 }
--- MakersMassiveJuiceIt/Assets/!Scripts/Managers/SliderManager.cs
+++ MakersMassiveJuiceIt/Assets/!Scripts/Managers/SliderManager.cs
@@ -1,16 +1,58 @@
+using System.Collections;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class SliderManager : MonoBehaviour
 {
+
+    [SerializeField] private Slider sldJuice;
+    [SerializeField] private float drainDuration = 3;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine("drainBar");
+        }
+        IncrementSlider(0.002f);
     }
+
+    public void IncrementSlider(float fruitVal)
+    {
+        if (sldJuice.value >= sldJuice.maxValue)
+        {
+            StartCoroutine(drainBar());
+        }
+        else
+        {
+            sldJuice.value += fruitVal;
+        }
+
+    }
+
+    public IEnumerator drainBar()
+    {
+        float startValue = sldJuice.value;
+        float elapsedTime = 0f;
+
+        //yield return new WaitForSecondsRealtime(5);
+
+        while (elapsedTime < drainDuration)
+        {
+            sldJuice.value = Mathf.Lerp(startValue, 0, elapsedTime / drainDuration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        sldJuice.value = 0; 
+    }
+
+
+
+
 }
